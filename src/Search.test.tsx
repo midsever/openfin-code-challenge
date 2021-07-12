0ecb9f2ebea5bc12ee1ece95aaa6cc708c7f468f
David Gardner <david.gardner@tcg.com>
2021-07-11 22:44:17 -0400

    Basic layout and animations

---
+++ src/Search.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Search from './Search';
+
+test('renders Search component', () => {
+  render(<Search />);
+});
